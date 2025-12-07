30f4a27307de55285665bc11c6ad54d86b5df76f
Shanthi Sharma <poojaarjun2579@gmail.com>
2025-12-07 23:23:13 +0530

    add two number file

---
+++ python-essential-training-4314028-main/exercise_files/Add_two_numbers.py
@@ -0,0 +1,10 @@
+#get input from users
+num1 = float(input("Enter num 1"))
+num2 = float(input("Enter num 2"))
+
+#add 2 numbers
+sum=num1 + num2
+
+#print the two numbers
+
+print("sum of your number is", " ", sum)
